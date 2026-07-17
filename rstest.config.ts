51325865b30a9e3b1933c247bfd22370b53489e8
neverland <chenjiahan.jait@bytedance.com>
2026-07-17 20:47:22 +0800

    test: increase test timeout

--- rstest.config.ts
+++ rstest.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from '@rstest/core';
 
 export default defineConfig({
+  testTimeout: 10_000,
   env: {
     // Let Rsbuild choose the mode based on the command.
     NODE_ENV: undefined,
