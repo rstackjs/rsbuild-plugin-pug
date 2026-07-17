9499df110277c03713c0c079f8f0511b28def899
neverland <chenjiahan.jait@bytedance.com>
2026-07-17 21:27:20 +0800

    fix(deps): update dependency reduce-configs to v2

--- test/basic/rsbuild.config.ts
+++ test/basic/rsbuild.config.ts
@@ -1,7 +1,14 @@
+import { fileURLToPath } from 'node:url';
 import { pluginPug } from '@rsbuild/plugin-pug';
 
 export default {
-  plugins: [pluginPug()],
+  plugins: [
+    pluginPug({
+      pugOptions: {
+        basedir: fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    }),
+  ],
   html: {
     template: './src/index.pug',
   },
